8af624a1d4c53e252bdf2f0360357ba6ad35916f
Alexander <ahansen1335@gmail.com>
2022-06-09 07:01:36 +0200

    create unstyled nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+        <h3>Logo</h3>
+        <ul className="nav-links">
+            <Link to="/">
+                <li>Home</li>
+            </Link>
+            <Link to="/about">
+                <li>About</li>
+            </Link>
+            <Link to="/shop">
+                <li>Shop</li>
+            </Link>
+            <Link to="/cart">
+                <li>Cart-icon</li>
+            </Link>
+        </ul>
+    </nav>
   )
 }
 
