a6c88f3d972e7ce9d0659b93ea5adbb7add89885
Alexander <ahansen1335@gmail.com>
2022-06-09 07:32:32 +0200

    add logo link to home page with appropriate style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <nav>
-        <h3>Logo</h3>
+        <Link style={{'text-decoration': 'none'}} to="/">
+            <h3 className="logo">Logo</h3>
+        </Link>
         <ul className="nav-links">
             <Link to="/">
                 <li>Home</li>
